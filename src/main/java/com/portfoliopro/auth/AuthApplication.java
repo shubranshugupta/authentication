0e8d8a987441ae559cb10c8aaef7d8742d1e0fd3
shubhranshugupta <shubranshugupta@gmail.com>
2024-05-06 22:58:24 +0530

    added todo function

--- src/main/java/com/portfoliopro/auth/AuthApplication.java
+++ src/main/java/com/portfoliopro/auth/AuthApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AuthApplication {
-
+	// todo: add cache functionality to reduce the number of database calls
 	public static void main(String[] args) {
 		SpringApplication.run(AuthApplication.class, args);
 	}
