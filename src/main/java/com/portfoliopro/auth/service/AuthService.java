4ce338b267c5a439a22d90f259985e6a4a01ed9d
shubhranshugupta <shubranshugupta@gmail.com>
2024-05-08 22:11:45 +0530

    remove unwanted imports

--- src/main/java/com/portfoliopro/auth/service/AuthService.java
+++ src/main/java/com/portfoliopro/auth/service/AuthService.java
@@ -1,9 +1,7 @@
 package com.portfoliopro.auth.service;
 
-import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -15,49 +13,46 @@ import com.portfoliopro.auth.entities.Role;
 import com.portfoliopro.auth.entities.User;
 import com.portfoliopro.auth.repository.UserRepository;
 
-// import io.jsonwebtoken.JwtException;
-import io.jsonwebtoken.io.DecodingException;
-import io.jsonwebtoken.security.InvalidKeyException;
-import io.jsonwebtoken.security.WeakKeyException;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final JwtService jwtService;
-    private final AuthenticationManager manager;
+        private final UserRepository userRepository;
+        private final PasswordEncoder passwordEncoder;
+        private final JwtService jwtService;
+        private final AuthenticationManager manager;
 
-    public AuthResponse registerUser(RegisterRequest request) {
-        User user = User.builder()
-                .firstName(request.getFirstName())
-                .lastName(request.getLastName())
-                .email(request.getEmail())
-                .password(passwordEncoder.encode(request.getPassword()))
-                .role(Role.USER)
-                .build();
+        public AuthResponse registerUser(RegisterRequest request) {
+                User user = User.builder()
+                                .firstName(request.getFirstName())
+                                .lastName(request.getLastName())
+                                .email(request.getEmail())
+                                .password(passwordEncoder.encode(request.getPassword()))
+                                .role(Role.USER)
+                                .build();
 
-        userRepository.save(user);
+                userRepository.save(user);
 
-        String token = jwtService.generateToken(user);
-        return AuthResponse.builder()
-                .token(token)
-                .build();
-    }
+                String token = jwtService.generateToken(user);
+                return AuthResponse.builder()
+                                .token(token)
+                                .build();
+        }
 
-    public AuthResponse loginUser(LoginRequest request) {
-        User user = userRepository.findByEmail(request.getEmail())
-                .orElseThrow(() -> new UsernameNotFoundException(request.getEmail() + " user not found"));
+        public AuthResponse loginUser(LoginRequest request) {
+                User user = userRepository.findByEmail(request.getEmail())
+                                .orElseThrow(() -> new UsernameNotFoundException(
+                                                request.getEmail() + " user not found"));
 
-        manager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        request.getEmail(),
-                        request.getPassword()));
+                manager.authenticate(
+                                new UsernamePasswordAuthenticationToken(
+                                                request.getEmail(),
+                                                request.getPassword()));
 
-        String token = jwtService.generateToken(user);
-        return AuthResponse.builder()
-                .token(token)
-                .build();
-    }
+                String token = jwtService.generateToken(user);
+                return AuthResponse.builder()
+                                .token(token)
+                                .build();
+        }
 }
