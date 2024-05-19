ce7f09a18348ff79d2f13fa820150e1ecc6297ab
shubhranshugupta <shubranshugupta@gmail.com>
2024-05-20 01:25:59 +0530

    added todo

--- src/main/java/com/portfoliopro/auth/AuthApplication.java
+++ src/main/java/com/portfoliopro/auth/AuthApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AuthApplication {
 	// todo: add cache functionality to reduce the number of database calls
 	// todo: use proper http code for each exception
-	// todo: need to handle email body cretion in a better way
+	// todo: need to handle email body cretion in a better way in event listner
 	public static void main(String[] args) {
 		SpringApplication.run(AuthApplication.class, args);
 	}
